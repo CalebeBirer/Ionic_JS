1744d2ce3d0b8cef17825de19b424c258f1358b4
Calebe Birer <calebeocanha53@gmail.com>
2024-04-30 21:20:56 -0300

    project

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,28 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  peso:number = 0;
+  altura:number = 0;
+  imc:number = 0;
+
+  calcular(){
+    if(this.peso !== undefined && this.altura !== undefined){
+      this.imc = this.peso / (this.altura * this.altura);
+    }
+    else{
+      this.imc = 0;
+    }
+  }
+
+  constructor(public toastcontroller : ToastController) {}
+
+  async exibirMensagem(){
+    const toast = await this.toastcontroller.create({
+      message:"Atencao voce clicou em gravar!",
+      duration:5000
+    })
+    toast.present();
+  }
+  
 
 }
